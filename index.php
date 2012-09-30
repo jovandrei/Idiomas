73cd97644cd0998ed41437f2aff8b22ed753bce7
Andrei Tapia <andrei13@gmail.com>
2012-09-29 20:45:24 -0500

    Comente linea que marcaba problema en theme.css. Archive los archivos de index en un solo folder.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html dir="ltr" lang="en-US" class=" webkit chrome win js webkit chrome win js">
-	<?php include('head.php');?>
+	<?php include('indexSection/head.php');?>
 	<body class="page page-id-37 page-template page-template-template-event-manager-my-classes-php">
-		<?php include('main.php');?>		
-		<?php include('scripts.php');?>
+		<?php include('indexSection/main.php');?>		
+		<?php include('indexSection/scripts.php');?>
 	</body>
 </html>
